6ec50e2861c5f74af7a9b7bb28d9bbb101e179b5
Hiuri Mendes <hiurimendes@gmail.com>
2024-05-17 10:43:55 -0300

    Componente: Header

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  await new Promise((resolve) => setTimeout(resolve, 200))
 
   return <h1>Hello world</h1>
 }
