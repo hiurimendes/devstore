55c40987fc5a5c38cb93be5e4ac74edc8ca11938
Hiuri Mendes <hiurimendes@gmail.com>
2024-05-20 08:59:48 -0300

    Fetch de dados no Produto

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+import data from '../data.json'
+import { NextResponse } from 'next/server'
+
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } },
+) {
+  console.log(params.slug)
+
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const slug = z.string().parse(params.slug)
+
+  const product = data.products.find((product) => product.slug === slug)
+
+  if (!product) {
+    return NextResponse.json({ message: 'Product not found.' }, { status: 400 })
+  }
+
+  return Response.json(product)
+}
