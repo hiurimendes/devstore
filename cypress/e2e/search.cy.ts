19b0e6cd092acd79c4c2195660ee943f2e2d7b16
Hiuri Mendes <hiurimendes@gmail.com>
2024-05-21 10:57:51 -0300

    Testando a busca

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,25 @@
+describe('search products', () => {
+  it('should be able to search for products', () => {
+    cy.visit('/')
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.wait(1000)
+
+    cy.location('pathname').should('include', '/search')
+    cy.location('search').should('include', 'q=moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should not be able to visit search page without a search query ', () => {
+    cy.on('uncaught:exception', () => {
+      return false
+    })
+
+    cy.visit('/search')
+
+    cy.wait(1000)
+
+    cy.location('pathname').should('equal', '/')
+  })
+})
